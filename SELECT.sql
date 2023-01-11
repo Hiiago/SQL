d96daa31114773535fea434ba10b67090cf66b77
finslan <finslan8@gmail.com>
2023-01-11 11:55:42 -0300

    primeiro commit

---
+++ SELECT.sql
@@ -0,0 +1,15 @@
+SELECT
+-- SQL Server, Postgres, Oracle, mySql
+
+SELECT coluna1,coluna2
+FROM tabela
+
+SELECT *
+FROM tabela
+
+-- WHERE
+
+SELECT coluna1,coluna2,coluna3
+FROM tabela
+WHERE condicao;
+
